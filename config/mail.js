90b97a16e09c2d5be471e4efbec7f1013ba08cc6
Yuvanjeyan <yuvanyvgs5454@gmail.com>
2026-04-02 19:15:06 +0530

    Nodemailer port defined

--- config/mail.js
+++ config/mail.js
@@ -6,7 +6,9 @@ exports.sendEmail = async (to, subject, html) => {
   }
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
