8d21419ee870d48da80c510adb877dc96969debe
Yuvanjeyan <yuvanyvgs5454@gmail.com>
2026-04-05 22:58:28 +0530

    Added to check- sendgrid

--- config/mail.js
+++ config/mail.js
@@ -1,27 +1,24 @@
-const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail');
 
 exports.sendEmail = async (to, subject, html) => {
-  if (!process.env.EMAIL || !process.env.PASSWORD) {
-    throw new Error("Email service is not configured. Set EMAIL and PASSWORD.");
+  if (!process.env.SENDGRID_API_KEY) {
+    throw new Error("Email service is not configured. Set SENDGRID_API_KEY.");
   }
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
-  });
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  await transporter.verify();
+  if (!process.env.FROM_EMAIL) {
+    throw new Error(
+      "SendGrid sender email is not configured. Set FROM_EMAIL to your verified SendGrid sender."
+    );
+  }
 
-  await transporter.sendMail({
-    from: process.env.EMAIL,
+  const msg = {
     to,
+    from: process.env.FROM_EMAIL,
     subject,
     html,
-  });
+  };
+
+  await sgMail.send(msg);
 };
